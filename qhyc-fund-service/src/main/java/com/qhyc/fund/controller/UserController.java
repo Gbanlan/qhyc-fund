deef2e799ab9516f8684280480c5d8912e1d646b
seven-mz <zhaomingyang90@163.com>
2017-06-02 16:41:13 +0800

    MVC支持JSON

--- qhyc-fund-service/src/main/java/com/qhyc/fund/controller/UserController.java
+++ qhyc-fund-service/src/main/java/com/qhyc/fund/controller/UserController.java
@@ -6,21 +6,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.qhyc.fund.entity.User;
 import com.qhyc.fund.service.UserService;
 
 
 @Controller
+@RequestMapping("/user/")
 public class UserController {
 
 	@Resource
 	private UserService userService;
 	
-	@RequestMapping(value="/index/{id}")
-	 public String goToIndexJsp(@PathVariable int id){
+	@RequestMapping(value="goHome/{id}")
+	 public @ResponseBody User goHome(@PathVariable int id){
 		User user=userService.findById(id);
 		System.out.println(user.getName());
-		return "home";
+		return user;
 	 }
 }
